1d10b4d4f02ec843c0c6723ab42e2a8331a62a43
Sahaj Sawhney <sahaj.sawhney@gmail.com>
2014-02-16 20:21:43 +0000

    Initial Commit

---
+++ ObjectDetection/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ObjectDetection
+//
+//  Created by Sahaj Sawhney on 16/02/14.
+//  Copyright (c) 2014 Sahaj Sawhney. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
