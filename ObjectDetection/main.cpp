37c3741aafefcc841f70a03bcdf964df5bed242b
Sahaj Sawhney <sahaj.sawhney@gmail.com>
2014-02-18 16:55:45 +0000

    initial commit

--- ObjectDetection/main.cpp
+++ ObjectDetection/main.cpp
@@ -1,18 +1,58 @@
-//
-//  main.cpp
-//  ObjectDetection
-//
-//  Created by Sahaj Sawhney on 16/02/14.
-//  Copyright (c) 2014 Sahaj Sawhney. All rights reserved.
-//
+#include "opencv2/opencv.hpp"
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 
 #include <iostream>
+#include <stdio.h>
 
-int main(int argc, const char * argv[])
+using namespace std;
+using namespace cv;
+
+/** Function Headers */
+void detectAndSave( Mat frame );
+
+/** Global variables */
+String logo_cascade_name = "haarcascade_frontalface_default.xml";
+
+CascadeClassifier logo_cascade;
+
+string window_name = "Capture - Face detection";
+
+/** @function main */
+int main( int argc, const char** argv )
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+//	CvCapture* capture;
+	Mat frame = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+    
+	//-- 1. Load the cascades
+	//logo_cascade.load(logo_cascade_name);
+	if( !logo_cascade.load( logo_cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
+    
+	detectAndSave( frame );
+    
+	return 0;
 }
 
+/** @function detectAndSave */
+void detectAndSave( Mat frame )
+{
+	std::vector<Rect> faces;
+	Mat frame_gray;
+    
+	cvtColor( frame, frame_gray, CV_BGR2GRAY );
+	equalizeHist( frame_gray, frame_gray );
+    
+	//-- Detect faces
+	logo_cascade.detectMultiScale( frame_gray, faces, 1.1, 1, 0|CV_HAAR_SCALE_IMAGE, Size(50, 50), Size(500,500) );
+	std::cout << faces.size() << std::endl;
+    
+	for( int i = 0; i < faces.size(); i++ )
+	{
+		rectangle(frame, Point(faces[i].x, faces[i].y), Point(faces[i].x + faces[i].width, faces[i].y + faces[i].height), Scalar( 0, 255, 0 ), 2);
+	}
+    
+	//-- Save what you got
+	imwrite( "output.jpg", frame );
+    
+}
